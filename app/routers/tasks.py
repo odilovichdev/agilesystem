db8592cdc3eb3b3c6e428c884cf0ae2e4b73d9df
odilovichdev <fazliddinn.gadoyev@gmail.com>
2025-07-23 14:53:38 +0500

    add: task endpoints

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,9 +1,19 @@
+from typing import List
+
 from fastapi import APIRouter, HTTPException
+from sqlalchemy.orm import selectinload
 
 
 from app.models import Task, Project, ProjectMemmber
 from app.dependencies import db_dep, project_manager_dep
-from app.schemas.tasks import TaskCreateIn, TaskCreateOut
+from app.schemas.tasks import (
+    TaskCreateIn, 
+    TaskCreateOut, 
+    TaskListOut,
+    TaskEditIn,
+    TaskEditOut,
+    TaskDetailOut
+)
 
 router = APIRouter(
     prefix="/tasks",
@@ -59,7 +69,94 @@ async def task_create(db: db_dep, user: project_manager_dep, task_in: TaskCreate
 
     return new_task
     
+    
+@router.get("/list/", response_model=List[TaskListOut])
+async def task_list(db: db_dep):
+    
+    tasks = (
+        db.query(Task).
+        options(
+            # load_only(Task.id, Task.key, Task.summary, Task.description, Task.priority, Task.due_date),
+            selectinload(Task.project),
+            selectinload(Task.status),
+            selectinload(Task.assignee),
+            selectinload(Task.reporter)
+        )
+        .all()
+    )
 
+    # tasks = db.query(Task).all()
+
+    return tasks
+
+
+@router.delete("/{task_id:int}/delete/")
+async def delete_task(db: db_dep, task_id: int):
+    task = db.query(Task).filter(Task.id==task_id).first()
+
+    if not task:
+        raise HTTPException(
+            detail="Task not found.",
+            status_code=404
+        )
+    
+    db.delete(task)
+    db.commit()
+
+    return {
+        "success": True,
+        "message": "Task delete successfully"
+    }
+
+
+@router.put("/{task_id:int}/update/", response_model=TaskEditOut)
+async def edit_task(db: db_dep, task_id: int, user: project_manager_dep, task_in: TaskEditIn):
+    task = db.query(Task).filter(Task.id==task_id).first()
+
+    if not task:
+        raise HTTPException(
+            detail=f"Task with {task_id} not found.",
+            status_code=404
+        )
+
+    project_member = db.query(ProjectMemmber).filter(
+        ProjectMemmber.user_id==user.id, 
+        ProjectMemmber.project_id==task.project_id
+        ).first()
+
+    if not project_member:
+        raise HTTPException(
+            detail="Siz bu task yaratilgan loyihaga biriktirilmagansiz.",
+            status_code=403
+        )
+    
+    if not db.query(Task).filter(Task.reporter_id==user.id).first():
+        raise HTTPException(
+            detail="Siz bu task ni yaratmagansiz.",
+            status_code=403
+        )
+    
+    update_task = task_in.model_dump(exclude_unset=True)
+
+    for key, value in update_task.items():
+        setattr(task, key, value)
+    
+    db.commit()
+    db.refresh(task)
+
+    return task
     
     
+
+@router.get("/{task_id:int}/", response_model=TaskDetailOut)
+async def task_detail(db: db_dep, task_id: int):
+    task = db.query(Task).filter(Task.id==task_id).first()
+
+    if not task:
+        raise HTTPException(
+            detail="Task topilmadi.",
+            status_code=404
+        )
     
+    return task
+
--- app/schemas/tasks.py
+++ app/schemas/tasks.py
@@ -26,7 +26,6 @@ class TaskCreateIn(BaseModel):
     assignee_id: int
 
 
-
 class TaskCreateOut(BaseModel):
     key: str
     summary: str
@@ -42,4 +41,35 @@ class TaskCreateOut(BaseModel):
         "from_attributes": True
     }
 
-    
+
+class TaskListOut(BaseModel):
+    id: int
+    key: str
+    summary: str
+    description: str | None = None
+    priority: Priority = Priority.LOW.value
+    due_date: datetime
+
+    project: ProjectCreateOut
+    status: StatusCreateOut
+    assignee: UserListOut
+    reporter: UserListOut
+
+    model_config = {
+        "from_attributes": True
+    }
+
+
+class TaskEditIn(BaseModel):
+    key: str | None = None
+    summary: str | None = None
+    description: str | None = None
+    due_date: datetime | None = None
+
+
+class TaskEditOut(TaskCreateIn):
+    ...
+
+
+class TaskDetailOut(TaskListOut):
+    ...
